b3e96c5a06156c4f52505ce58351bec9db73e403
Robert Doherty <robert.doherty@visualrevenue.com>
2013-05-20 07:52:08 -0400

    added key for starting chrome

--- config.py
+++ config.py
@@ -41,73 +41,74 @@ mod = "mod4"
 # The keys variable contains a list of all of the keybindings that qtile will
 # look through each time there is a key pressed.
 keys = [
-    # Log out; note that this doesn't use mod4: that's intentional in case mod4
-    # gets hosed (which happens if you unplug and replug your usb keyboard
-    # sometimes, or on system upgrades). This way you can still log back out
-    # and in gracefully.
-    Key(["shift", "mod1"], "q",  lazy.shutdown()),
+	# Log out; note that this doesn't use mod4: that's intentional in case mod4
+	# gets hosed (which happens if you unplug and replug your usb keyboard
+	# sometimes, or on system upgrades). This way you can still log back out
+	# and in gracefully.
+	Key(["shift", "mod1"], "q",  lazy.shutdown()),
 
-    Key([mod], "k",              lazy.layout.down()),
-    Key([mod], "j",              lazy.layout.up()),
-    Key([mod], "h",              lazy.layout.previous()),
-    Key([mod], "l",              lazy.layout.previous()),
-    Key([mod, "shift"], "space", lazy.layout.rotate()),
-    Key([mod, "shift"], "Return",lazy.layout.toggle_split()),
-    Key(["mod1"], "Tab",         lazy.nextlayout()),
-    Key([mod], "x",              lazy.window.kill()),
+	Key([mod], "k",              lazy.layout.down()),
+	Key([mod], "j",              lazy.layout.up()),
+	Key([mod], "h",              lazy.layout.previous()),
+	Key([mod], "l",              lazy.layout.previous()),
+	Key([mod, "shift"], "space", lazy.layout.rotate()),
+	Key([mod, "shift"], "Return",lazy.layout.toggle_split()),
+	Key(["mod1"], "Tab",         lazy.nextlayout()),
+	Key([mod], "x",              lazy.window.kill()),
 
-    # interact with prompts
-    Key([mod], "r",              lazy.spawncmd()),
-    Key([mod], "g",              lazy.switchgroup()),
+	# interact with prompts
+	Key([mod], "r",              lazy.spawncmd()),
+	Key([mod], "g",              lazy.switchgroup()),
 
-    # start specific apps
-    Key([mod], "n",              lazy.spawn("firefox")),
-    Key([mod], "Return",         lazy.spawn("xterm")),
+	# start specific apps
+	Key([mod], "n",              lazy.spawn("firefox")),
+	Key([mod], "c",              lazy.spawn("google-chrome")),
+	Key([mod], "Return",         lazy.spawn("xterm")),
 
-    # Change the volume if your keyboard has special volume keys.
-    Key(
-        [], "XF86AudioRaiseVolume",
-        lazy.spawn("amixer -c 0 -q set Master 2dB+")
-    ),
-    Key(
-        [], "XF86AudioLowerVolume",
-        lazy.spawn("amixer -c 0 -q set Master 2dB-")
-    ),
-    Key(
-        [], "XF86AudioMute",
-        lazy.spawn("amixer -c 0 -q set Master toggle")
-    ),
+	# Change the volume if your keyboard has special volume keys.
+	Key(
+		[], "XF86AudioRaiseVolume",
+		lazy.spawn("amixer -c 0 -q set Master 2dB+")
+	),
+	Key(
+		[], "XF86AudioLowerVolume",
+		lazy.spawn("amixer -c 0 -q set Master 2dB-")
+	),
+	Key(
+		[], "XF86AudioMute",
+		lazy.spawn("amixer -c 0 -q set Master toggle")
+	),
 
-    # Also allow changing volume the old fashioned way.
-    Key([mod], "equal", lazy.spawn("amixer -c 0 -q set Master 2dB+")),
-    Key([mod], "minus", lazy.spawn("amixer -c 0 -q set Master 2dB-")),
+	# Also allow changing volume the old fashioned way.
+	Key([mod], "equal", lazy.spawn("amixer -c 0 -q set Master 2dB+")),
+	Key([mod], "minus", lazy.spawn("amixer -c 0 -q set Master 2dB-")),
 ]
 
 # This allows you to drag windows around with the mouse if you want.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-        start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-        start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+	Drag([mod], "Button1", lazy.window.set_position_floating(),
+	     start=lazy.window.get_position()),
+	Drag([mod], "Button3", lazy.window.set_size_floating(),
+	     start=lazy.window.get_size()),
+	Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
 # Next, we specify group names, and use the group name list to generate an appropriate
 # set of bindings for group switching.
 groups = []
 for i in ["a", "s", "d", "f", "u", "i", "o", "p"]:
-    groups.append(Group(i))
-    keys.append(
-        Key([mod], i, lazy.group[i].toscreen())
-    )
-    keys.append(
-        Key([mod, "mod1"], i, lazy.window.togroup(i))
-    )
+	groups.append(Group(i))
+	keys.append(
+		Key([mod], i, lazy.group[i].toscreen())
+	)
+	keys.append(
+		Key([mod, "mod1"], i, lazy.window.togroup(i))
+	)
 
 # Two basic layouts.
 layouts = [
-    layout.Stack(stacks=2, border_width=1),
-    layout.Max(),
+	layout.Stack(stacks=2, border_width=1),
+	layout.Max(),
 ]
 
 # vim: tabstop=4 shiftwidth=4 expandtab
